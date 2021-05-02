1d277b8ae08ea281958959a1ed22e2bb2706f164
jaehyuen <violet6520@naver.com>
2021-05-02 21:16:56 +0900

    [2021/05/02 이재현] 어노테이션  옵션 정리
    1. Table, Column 관련 어노테션 옵션 정리

--- study/src/main/java/com/study/jpa/entity/TeacherEntity.java
+++ study/src/main/java/com/study/jpa/entity/TeacherEntity.java
@@ -16,8 +16,36 @@ import lombok.NoArgsConstructor;
 
 @Data
 @EqualsAndHashCode(callSuper=true)
+/**
+ * @Entity 어노테이션 옵션
+ * 
+ * name (String) = jpa에서 사용할 이름 설정, 기본값은 클래스명
+ *
+ */
 @Entity
-@Table(name = "teacher")
+
+/**
+ * @Table 어노테이션 옵션
+ * 
+ * name (String)                           = 엔티티와 맵핑할 테이블 명, 기본값은 엔티티 명
+ * catalog (String)                        = catalog 기능이 있는 디비에서 catalog를 맵핑, 기본값은 기본 catalog사용
+ * schema (String)                         = schema 기능이 있는 디비에서 schema를 맵핑, 기본값은 기본 schema사용
+ * indexes (Index[])                       = 테이블에 대한 인덱스 설정, 기본키 인덱스는 자동으로 생성됨
+ * uniqueConstraints (UniqueConstraints[]) = 테이블 생성시 unique 제약조건 설정, 스키마 자동생성 기능을 사용할떄만 가능
+ * 
+ * @Index 어노테이션 옵션
+ * 
+ * name (String)       = 인덱스명 설정
+ * columnList (String) = 인덱스에 사용되는 컬럼을 순서대로 작성 (필수값)
+ * unique (boolean)    = 인덱스의 unique여부 설정
+ *  
+ * @UniqueConstraint 어노테이션 옵션
+ * 
+ * name (String)          = 이름설정인데 정확한건 모르겟음..
+ * columnNames (String[]) = unique 설정할 컬럼 리스트 (필수값)
+ * 
+ */
+@Table(name ="teacher")
 @AllArgsConstructor
 @NoArgsConstructor
 public class TeacherEntity extends BaseEntity {
@@ -33,6 +61,27 @@ public class TeacherEntity extends BaseEntity {
 	 */
 	
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	
+	/**
+	 * @Column 어노테이션 옵션
+	 * 
+	 * columnDefinition (String) = 컬럼 정보를 직접 작성, 기본적으로 생성한 sql에 유추해서 집어넣는다..?
+	 * name (String)             = 변수명과 맵핑할 디비 컬럼명을 설정, 기본값은 필드명이다
+	 * 
+	 * insertable (boolean)      = 디비 컬럼 등록 가능 여부, 기본값은  true
+	 * updateable (boolean)      = 디비 컬럼 수정 가능 여부, 기본값 은 true
+	 * 
+	 * nullable (boolean)        = 디비 컬럼에 not null 제약조건 설정
+	 * unique (boolean)          = 디비 컬럼에 unique 제약조건 설정
+	 * table (String)            = 다른테이블과 맵핑시 사용
+	 * 
+	 * length (int)              = 문자열 길이 설정(문자열 관련 컬럼에만 사용), 기본값은 255
+	 * precision (int)           = 소수점을 포함한 전체 자리수 설정 (demical 컬럼에서만 사용)
+	 * scale (int)               = 소수점의 자리수 설정 (demical 컬럼에서만 사용)
+	 * 
+	 * ※ demical 컬럼은 자바에서 BigDecimal로 선언해야됨
+	 * 
+	 */
 	@Column(name = "TEACHER_SEQ") // 컬럼 이름 설정
 	private Long teacherSeq;
 	
